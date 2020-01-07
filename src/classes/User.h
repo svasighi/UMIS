0f861907f3b35ded576666739f08b80d2d1985c2
shadmehr vasighi <49784686+svasighi@users.noreply.github.com>
2020-01-08 00:09:46 +0330

    Update User.h

--- src/classes/User.h
+++ src/classes/User.h
@@ -1,24 +1,25 @@
+#include <iostream>
+#include <string>
+
 #ifndef USER_H
 #define USER_H
 
-#include <string>
-
 // .............................FIELD......................................
-class Field { 
+class Field{ 
     int ID;
-    std::string fieldname;
+    std::string FieldName;
 public:
-    inline void setID(int _fieldID){ ID = _fieldID;}
-    inline std::string getID(void) const {return ID;}
-    inline void setFieldName(std::string _fieldname){fieldname = _fieldname;}
-    inline std::string getFieldName(void) const {return fieldname;}
+    inline void SetID(int FieldID){ ID = FieldID;}
+    inline std::String GetID(void) const {return FieldName;}
+    inline void SetFieldName(std::String fieldname){FieldName = fieldname;}
+    inline std::String GetFieldName(void) const {return FieldName;}
 };
 // .............................USER......................................
 
 class User
 {
 private:
-    unsigned int ID;
+    unsigned int user_name;
     std::string first_name;
     std::string last_name;
     std::string password;
@@ -26,18 +27,17 @@ private:
 public:
     User(){}; // it won't do anything :)
     ~User(){}; // it won't do anything :)
-    inline void setID(const unsigned);
-    inline unsigned int getID(void) const {return ID;}
-    inline void setFirstName(const std::string); //forward definition
-    inline std::string getFirstName(void) const {return first_name;}
-    inline void setLastName(const std::string); //forward definition
-    inline std::string getLastName(void) const {return last_name;}
-    inline void setField(const Field); //forward definition
-    inline Field getField(void) const {return field;};    
-    inline void setPassword(const std::string); //forward definition
-    inline void std::string getPassword(void) const {return password;}
-    inline void CheckPassword(); //forward definition
+    void Setuser_name(const user_name);
+    inline unsigned Getuser_name(void) const {return user_name;};
+    void Setfirst_name(const std::string); //forward definition
+    std::string Getfirst_name(void) const {return first_name;};
+    void Setlast_name(const std::string); //forward definition
+    std::string Getlast_name(void) const {return last_name;};
+    void SetField(const Field); //forward definition
+    Field GetField(void) const {return field;};    
+    void SetPassword(const std::string); //forward definition
+    void CheckPassword(); //forward definition
     virtual void ChangePassword(); //forward definition
 };
 
-#endif // USER_H
+#endif
