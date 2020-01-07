e3df4d11f1130f475e2d06facefc051d33f7021d
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-07 20:19:51 +0330

    Update Professor.h

--- src/classes/Professor.h
+++ src/classes/Professor.h
@@ -1,31 +1,25 @@
 #include <iostream>
 #include "AcademicAffairs.h"
  
-class Professor{
+class Professor {
 
 };
+
 class AdjunctProfessor : public Professor { 
 
-void ChangePassword(){
-    
-}
 // list of courses
 //access grades list
 
 
 };
 
-class Faculty : public AdjunctProfessor{
+class Faculty : public AdjunctProfessor {
 //is supervisor?? -> apply enrollments
 // degree
 
 };
-class DepartmentAcademicAffairsStaff :public AcademicAffairsStaff{
+class DepartmentAcademicAffairsStaff : public AcademicAffairsStaff {
 // list of all courses 
 // list of students of department
 
 };
-
-int main(){
-
-}
