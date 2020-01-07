ac2903505e4b84f41db90cea4a16e14769d0dbeb
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-07 21:55:34 +0330

    Update Student.h

--- src/classes/Student.h
+++ src/classes/Student.h
@@ -2,43 +2,18 @@
 #include <string>
 #include <vector>
 #include <map>
-#include "TimeDate.h"
-
-class Course {
-protected:
-	int credit;
-	std::string name;
-	int type;
-	std::vector<Course*> prerequisite;
-	std::vector<Course*> corequisite;
-};
-
-class Presented_Course : public Course {
-protected:
-	Professor* course_professor;
-	int capacity;
-	Course_Time course_time;
-	std::string course_location;
-	Time finalexam_time;
-};
-
-class Person {
-protected:
-	std::string username;
-	std::string password;
-	std::string firstName;
-	std::string lastName;
-public:
-	void setPassword(std::string _password);
-};
+#include "Course.h"
+#include "User.h"
 
 class MyTerm {
 protected:
 	int no;
 	std::map<Presented_Course*, float> courses;
+public:
+
 };
 
-class Student : public Person {
+class Student : public User {
 protected:
 	std::string field;
 	float grade;
@@ -53,21 +28,4 @@ class Guest_Student : public Student {
 public:
 	// getTuitionStatus();
 	// compute tuition
-};
-
-class Professor : public Person {
-protected:
-	std::vector<Presented_Course*> courses;
-	std::vector<Presented_Course*> course_students;
-};
-
-class Invited_Professor : public Professor {
-	
-};
-
-class Faculty : public Professor {
-protected:
-	int degree;
-	bool issupervisor;
-	std::vector<Student*> supervise_students;
 };
