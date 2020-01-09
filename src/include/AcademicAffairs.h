100d725a6e71e4f4c4fe47145c8559ff66b6c8ea
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-09 14:36:22 +0330

    Update AcademicAffairs.h

--- src/include/AcademicAffairs.h
+++ src/include/AcademicAffairs.h
@@ -1,18 +1,18 @@
 #ifndef ACADEMIC
 #define ACADEMIC
-#include <iostream>
-#include "..\models\AcademicAffairsModel.cpp"
 
+#include "..\models\AcademicAffairsModel.cpp"
 #include "User.h"
 
-class AcademicAffairsStaff :public User{
-    std::vector<Studnet*> students;
+class AcademicAffairsStaff : public User {
+    std::vector<Student*> students;
 
 };
-class WholeAcademicAffairsStaff :public AcademicAffairsStaff{
+class WholeAcademicAffairsStaff : public AcademicAffairsStaff {
 // list of all students 
     std::vector<Presented_Course*> courses;
 public:
     void ChangeDepartmentHead(Professor prof); //forward definitions
 };
+
 #endif // ACADEMIC
