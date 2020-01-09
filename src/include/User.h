717814fcd7288d38f259ce2020fd566254ec33cf
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-09 16:24:24 +0330

    Update User.h

--- src/include/User.h
+++ src/include/User.h
@@ -3,39 +3,23 @@
 
 #include <string>
 
-// .............................FIELD......................................
-class Field { 
-    int ID;
-    std::string FieldName;
-public:
-    inline void setID(int FieldID) { ID = FieldID;}
-    inline std::string getID(void) const {return FieldName;}
-    inline void setFieldName(std::string fieldname) {FieldName = fieldname;}
-    inline std::string setFieldName(void) const {return FieldName;}
-};
-// .............................USER......................................
-
-class User
-{
-private:
-    int user_name;
-    std::string first_name;
-    std::string last_name;
+class User {
+protected:
+    int username;
     std::string password;
-    Field field;
+    std::string firstname;
+    std::string lastname;
 public:
-    User(){}; // it won't do anything :)
-    ~User(){}; // it won't do anything :)
-    void setUserName(const int);
-    int getUserName(void) const {return user_name;}
-    void setFirstName(const std::string); //forward definition
-    std::string getFirstName(void) const {return first_name;}
-    void setLastName(const std::string); //forward definition
-    std::string getLastName(void) const {return last_name;}
-    void setField(const Field); //forward definition
-    Field getField(void) const {return field;}
-    void setPassword(const std::string); //forward definition
-    void CheckPassword(); //forward definition
+    User();
+    void setUserName(int _username);
+    int getUserName() const;
+    void setPassword(std::string _password);
+     std::string getPassword();
+    void setFirstName(std::string _firstname);
+    std::string getFirstName() const;
+    void setLastName(std::string _lastname);
+    std::string getLastName() const;
+    void CheckPassword();
     virtual void ChangePassword(); //forward definition
 };
 
