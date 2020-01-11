eb29f7b1804b4377a7074bce0c3576e3af399ec4
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-11 18:10:18 +0330

    Update AcademicAffairs.h

--- src/include/AcademicAffairs.h
+++ src/include/AcademicAffairs.h
@@ -4,16 +4,17 @@
 #include "User.h"
 #include "Student.h"
 
-class AcademicAffairsStaff : public User{
+class AcademicAffairsStaff : public User {
 protected:
 	std::string career;
 	std::vector<Student*> students;
 public:
-	std::vector<Student*> getStudents(void);
-	void setStudents(std::vector<Student *>);
-	std::string getCareer(void);
 	void setCareer(std::string);
+	std::string getCareer(void) const;
+	void setStudents(std::vector<Student*>);
+	std::vector<Student*> getStudents(void) const;
 };
+
 class TopAcademicAffairsStaff : public AcademicAffairsStaff {
 	
 public:
