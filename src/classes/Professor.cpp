cfdece2941214946ae3665462102d4bcbb141c69
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-11 17:41:36 +0330

    Update Professor.cpp

--- src/classes/Professor.cpp
+++ src/classes/Professor.cpp
@@ -1,60 +1,75 @@
 #include "../include/Professor.h"
 
-
 void Professor::setCourses(std::vector<Presented_Course*> _courses) {
     courses = _courses;
 }
+
 std::vector<Presented_Course*> Professor::getCourses() const {
     return courses;
 }
+
 void Professor::addCourse(Presented_Course* course) {
     courses.push_back(course);
 }
+
 void Professor::removeCourse(Presented_Course* course) {
     courses.erase(find(courses.begin(), courses.end(), course));
 }
 
-int Faculty::getDegree (void) const{
+int Faculty::getDegree (void) const {
     return degree;
 }
-void Faculty::setDegree(const int _degree){
+
+void Faculty::setDegree(const int _degree) {
     degree = _degree;
 }
-bool Faculty::isSupervisor (void) const{
+
+bool Faculty::isSupervisor (void) const {
     return isSupervisor;
 }
-void Faculty::setAsSupervisor(void){
+
+void Faculty::setAsSupervisor(void) {
     this->isSupervisor = true;
 }
-bool Faculty::isHead (void) const{
+
+bool Faculty::isHead (void) const {
     return isHead;
 }
-void Faculty::setAsHead(void){
+
+void Faculty::setAsHead(void) {
     this->isHead = true;
 }
+
 void Faculty::setSupervised_Students(std::vector<Student*> _supervised_students) {
     supervised_students = _supervised_students;
 }
-std::vector<Student*> Faculty::getSupervised_Students(void) const{
+
+std::vector<Student*> Faculty::getSupervised_Students(void) const {
     return supervised_students;
 }
-void Faculty::addSupervised_Students(Student* _supervised_student){
+
+void Faculty::addSupervised_Students(Student* _supervised_student) {
     supervised_students.push_back(_supervised_student);
 }
-void Faculty::removeSupervised_Students(Student* _supervised_student){
+
+void Faculty::removeSupervised_Students(Student* _supervised_student) {
     supervised_students.erase(find(supervised_students.begin(), supervised_students.end(), _supervised_student));
 }
+
 void Faculty::applyEnrollment(Student _student){} //forward definition
-std::vector<Presented_Course*> DepartmentAcademicAffairsStaff::getPresentedCourses() const{
+
+std::vector<Presented_Course*> DepartmentAcademicAffairsStaff::getPresentedCourses() const {
     return presented_courses;
 }
-std::vector<Course*> DepartmentAcademicAffairsStaff::getCourses()const{
+
+std::vector<Course*> DepartmentAcademicAffairsStaff::getCourses() const {
     return courses;
 }
-void DepartmentAcademicAffairsStaff::setCourses(std::vector<Course*> _courses){
+
+void DepartmentAcademicAffairsStaff::setCourses(std::vector<Course*> _courses) {
     courses = _courses;
 }
-void DepartmentAcademicAffairsStaff::setPresentedCourses(std::vector<Presented_Course*> _presented_courses){
+
+void DepartmentAcademicAffairsStaff::setPresentedCourses(std::vector<Presented_Course*> _presented_courses) {
     presented_courses = _presented_courses;
 }
-
