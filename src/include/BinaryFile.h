02d529f7d18c1cb61020f956f5683cab2158a735
shadmehr vasighi <49784686+svasighi@users.noreply.github.com>
2020-01-22 22:50:21 +0330

    add ,delete,update and select data

---
+++ src/include/BinaryFile.h
@@ -0,0 +1,24 @@
+/*
+	*******	IMPORTANT POINT : yout class must have int GetID(void); method ******** 
+	created by shadmehr vasighi under MIT lICENSE 
+	this code snippet is a part of the UMIS PROJECT
+	get in touch through my blog : https://svasighi.github.io
+
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+
+template<class Object>
+class BinaryFile{
+	char* file_address;
+public:
+    BinaryFile(char*);
+	~BinaryFile();
+    void AddRecord(Object);
+    Object* FetchAllRecords(int) const;
+    Object FetchByID(int) const;
+    void DeleteRecordByID(int);
+    void UpdateRecordByID(int, Object);
+
+};
