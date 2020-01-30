f7578286938c43a9c9df4da08118dea9d99b0d2f
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-30 22:50:11 +0330

    Update Student.cpp

--- src/classes/Student.cpp
+++ src/classes/Student.cpp
@@ -21,6 +21,22 @@ short MyCourse::getStatus() const {
 	return status;
 }
 
+void MyCourse::setAssessmentAnswers(std::vector<short> _assessment_answers) {
+	assessment_answers = _assessment_answers;
+}
+
+std::vector<short> MyCourse::getAssessmentAnswers() const {
+	return assessment_answers;
+}
+
+void MyCourse::setReview(std::string _review) {
+	review = _review;
+}
+
+std::string MyCourse::getReview() const {
+	return review;
+}
+
 
 
 
@@ -46,12 +62,12 @@ short MyTerm::getStatus() const {
 	return status;
 }
 
-void MyTerm::setRegistrationBeginTime(Time _registration_begin_time) {
-	registration_begin_time = _registration_begin_time;
+void MyTerm::setEnrollmentBeginTime(Time _enrollment_begin_time) {
+	enrollment_begin_time = _enrollment_begin_time;
 }
 
-Time MyTerm::getRegistrationBeginTime() const {
-	return registration_begin_time;
+Time MyTerm::getEnrollmentBeginTime() const {
+	return enrollment_begin_time;
 }
 
 void MyTerm::setCourses(std::map<Presented_Course*, MyCourse> _courses) {
@@ -62,6 +78,14 @@ std::map<Presented_Course*, MyCourse> MyTerm::getCourses() const {
 	return courses;
 }
 
+int MyTerm::numberofCredits() const {
+	int n = 0;
+	for (auto& x : courses) {
+		n += x.first->getCredit();
+	}
+	return n;
+}
+
 void MyTerm::addCourse(Presented_Course* course) {
 	courses[course] = MyCourse();
 }
@@ -70,22 +94,30 @@ void MyTerm::removeCourse(Presented_Course* course) {
 	courses.erase(course);
 }
 
+void MyTerm::setCourseProperties(Presented_Course* course, MyCourse properties) {
+	courses[course] = properties;
+}
+
+MyCourse MyTerm::getCourseProperties(Presented_Course* course) const {
+	if (courses.count(course) == 0)
+		throw std::invalid_argument("course not found");
+	return courses.at(course);
+}
+
 void MyTerm::setScoreofCourse(Presented_Course* course, float score) {
 	courses[course].setScore(score);
 }
 
-float MyTerm::getScoreofCourse(Presented_Course* course) const {
-	if (courses.count(course) == 0)
-		throw std::invalid_argument("course not found");
-	return courses.at(course).getScore();
+void MyTerm::setStatusofCourse(Presented_Course* course, short status) {
+	courses[course].setStatus(status);
 }
 
-int MyTerm::numberofcredits() const {
-	int n = 0;
-	for (auto& x : courses) {
-		n += x.first->getCredit();
-	}
-	return n;
+void MyTerm::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<short> answers) {
+	courses[course].setAssessmentAnswers(answers);
+}
+
+void MyTerm::setReviewofCourse(Presented_Course* course, std::string review) {
+	courses[course].setReview(review);
 }
 
 
@@ -123,12 +155,16 @@ void Student::addTerm(MyTerm term) {
 	terms[term.getno()] = term;
 }
 
-void Student::setScoreofCourse(Presented_Course* course, float score) {
-	terms[course->getTerm_no()].setScoreofCourse(course, score);
+void Student::addTerm(int term_no) {
+	terms[term_no] = MyTerm();
 }
 
-float Student::getScoreofCourse(Presented_Course* course) const {
-	return terms.at(course->getTerm_no()).getScoreofCourse(course);
+void Student::setTerm(int term_no, MyTerm term) {
+	terms[term_no] = term;
+}
+
+MyTerm Student::getTerm(int term_no) const {
+	return terms.at(term_no);
 }
 
 void Student::setTermStatus(int term_no, short status) {
@@ -139,12 +175,64 @@ short Student::getTermStatus(int term_no) const {
 	return terms.at(term_no).getStatus();
 }
 
-void Student::setTermRegistrationBeginTime(int term_no, Time registration_begin_time) {
-	terms[term_no].setRegistrationBeginTime(registration_begin_time);
+void Student::setTermEnrollmentBeginTime(int term_no, Time enrollment_begin_time) {
+	terms[term_no].setEnrollmentBeginTime(enrollment_begin_time);
 }
 
-Time Student::getTermRegistrationBeginTime(int term_no) const {
-	return terms.at(term_no).getRegistrationBeginTime();
+Time Student::getTermEnrollmentBeginTime(int term_no) const {
+	return terms.at(term_no).getEnrollmentBeginTime();
+}
+
+int Student::numberofCredits(int term_no) const {
+	return terms.at(term_no).numberofCredits();
+}
+
+void Student::addCourse(Presented_Course* course) {
+	terms[course->getTerm_no()].addCourse(course);
+}
+
+void Student::removeCourse(Presented_Course* course) {
+	terms[course->getTerm_no()].removeCourse(course);
+}
+
+void Student::setCourseProperties(Presented_Course* course, MyCourse properties) {
+	terms[course->getTerm_no()].setCourseProperties(course, properties);
+}
+
+MyCourse Student::getCourseProperties(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course);
+}
+
+void Student::setScoreofCourse(Presented_Course* course, float score) {
+	terms[course->getTerm_no()].setScoreofCourse(course, score);
+}
+
+float Student::getScoreofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getScore();
+}
+
+void Student::setStatusofCourse(Presented_Course* course, short status) {
+	terms[course->getTerm_no()].setStatusofCourse(course, status);
+}
+
+short Student::getStatusofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getStatus();
+}
+
+void Student::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<short> answers) {
+	terms[course->getTerm_no()].setAssessmentAnswersofCourse(course, answers);
+}
+
+std::vector<short> Student::getAssessmentAnswersofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getAssessmentAnswers();
+}
+
+void Student::setReviewofCourse(Presented_Course* course, std::string review) {
+	terms[course->getTerm_no()].setReviewofCourse(course, review);
+}
+
+std::string Student::getReviewofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getReview();
 }
 
 
@@ -163,5 +251,5 @@ short Tuition_Student::getType() const {
 }
 
 int Tuition_Student::computeTuition(int term_no) {
-	return terms[term_no].numberofcredits();// * credit_fee[type] + const_tuition;
+	return terms[term_no].numberofCredits();// * credit_fee[type] + const_tuition;
 }
