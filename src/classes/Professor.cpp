274a4e410f7a1645908d60132d0726091ce78eb7
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-31 19:33:46 +0330

    Update Professor.cpp

--- src/classes/Professor.cpp
+++ src/classes/Professor.cpp
@@ -75,16 +75,15 @@ std::vector<Presented_Course*> DepartmentAcademicAffairsStaff::getPresentedCours
 	return presented_courses;
 }
 // returns an int <= 5 which shows each professor assessment
-int DepartmentHead::CalculateProfessorAssessmentSum(Professor _professor) const{
+int DepartmentHead::CalculateProfessorAssessmentSum(Professor _professor) const {
 	std::vector<Presented_Course*> courses = _professor.getCourses();
 	int students_count = 0 , assessments_sum;
-	 for(int i=0; i < courses.size(); i++){
-		std::vector<Student*> students = (*courses[i]).getCourseStudents();
-		for(int j =0 ; j < students.size(); j++){
-			std::vector<short> assessment = (*students[j]).getAssessmentAnswersofCourse(courses[i]);
-			
+	 for (int i = 0; i < courses.size(); i++) {
+		std::vector<Student*> students = courses[i]->getCourseStudents();
+		for (int j = 0 ; j < students.size(); j++) {
+			std::vector<short> assessment = students[j]->getAssessmentAnswersofCourse(courses[i]);
 			int temp = 0;
-			for(int k =0 ; k<assessment.size(); k++)
+			for (int k =0 ; k < assessment.size(); k++)
 				temp += assessment[k];
 			assessments_sum += (temp / 5);
 			students_count++;			
@@ -93,65 +92,67 @@ int DepartmentHead::CalculateProfessorAssessmentSum(Professor _professor) const{
 	 return assessments_sum / students_count;
 }
 
-std::vector<Professor*> DepartmentHead::getProfessors()const{
-		return professors;
+std::vector<Professor*> DepartmentHead::getProfessors() const {
+	return professors;
 }
+
 void DepartmentHead::setProfessors(std::vector<Professor*> _professors) {
 	professors = _professors;
 }
-std::map<Presented_Course*, std::vector <short>> DepartmentHead::ProfessorAssessment(Professor _professor) const {
+
+std::map<Presented_Course*, std::vector<char>> DepartmentHead::ProfessorAssessment(Professor _professor) const {
 	std::vector<Presented_Course*> courses = _professor.getCourses();
 	std::map<Presented_Course*, std::vector <short>> assessments;
-	 for(int i=0; i < courses.size(); i++){
+	 for (int i = 0; i < courses.size(); i++) {
 		std::vector<Student*> students = (*courses[i]).getCourseStudents();
 		
 		for(int j =0 ; j < students.size(); j++){
 			std::vector<short> assessment_sum;
-			std::vector<short> assessment_temp = (*students[j]).getAssessmentAnswersofCourse(courses[i]);
-			for(int k =0 ; k< assessment_temp.size(); k++){
+			std::vector<short> assessment_temp = students[j]->getAssessmentAnswersofCourse(courses[i]);
+			for (int k = 0 ; k < assessment_temp.size(); k++) {
 				assessment_sum[k] += assessment_temp[k];
 				if (j == (students.size() - 1))
 					assessment_sum[k] /= 5;
 			}
-			assessments.insert(std::pair<Presented_Course*,std::vector <short>>(courses[i],assessment_sum));
+			assessments.insert(std::pair<Presented_Course*,std::vector <short>>(courses[i], assessment_sum));
 		}
 	 }
 }
-void DepartmentHead::addProfessor(int _username, std::string _password, std::string _firstname, std::string _lastname, int _departmentcode)  {
+void DepartmentHead::addProfessor(int _username, std::string _password, std::string _firstname, std::string _lastname) {
 
-	Professor * professor = new Professor(_username ,md5(_password) ,_firstname ,_lastname ,_departmentcode);
-	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat" );
+	Professor * professor = new Professor(_username, md5(_password), _firstname, _lastname, this->departmentcode);
+	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat");
 	binary_file.AddRecord(*professor);
 	professors.push_back(professor);
 }
 void DepartmentHead::deleteProfessor(Professor _professor)  {
 
-	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat" );
+	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat");
 	binary_file.DeleteRecordByID(_professor.getUserName());
 	professors.erase(find(professors.begin(), professors.end(), _professor));
 }
-void DepartmentHead::ReadAllProfessor()  {
+void DepartmentHead::ReadAllProfessors()  {
 	std::vector<Professor*> department_professors;
 	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat");
 	std::vector<Professor*> professors = binary_file.FetchAllRecords();
 	for(int i = 0 ; i < professors.size(); i++){
-		if((*professors[i]).getDepartmentCode() == departmentcode && (*professors[i]).getUserName() != username )
+		if(professors[i]->getDepartmentCode() == departmentcode && professors[i]->getUserName() != username )
 			department_professors.push_back(professors[i]);
 	}
 	setProfessors(department_professors);
 }
-void DepartmentHead::calcSalary(int* _degree_base_pay ,int _max_assesment_addition ) const {
+void DepartmentHead::calcSalary(int* _degree_base_pay, int _max_assesment_addition) const {
 	BinaryFile <Professor>binary_file((char*) "../storage/Professors.dat");
 	std::vector<Professor*> professors = binary_file.FetchAllRecords();
-	for(int i = 0 ; i < professors.size(); i++){
+	for (int i = 0; i < professors.size(); i++) {
 		Faculty* faculty = dynamic_cast<Faculty*> (professors[i]);
 		int salary = 0;
-		if(faculty){
-			salary = _degree_base_pay[(*faculty).getDegree()] + (CalculateProfessorAssessmentSum(*professors[i]) / 5) * _max_assesment_addition;
-		}else{
+		if (faculty) {
+			salary = _degree_base_pay[faculty->getDegree()] + (CalculateProfessorAssessmentSum(*professors[i]) / 5) * _max_assesment_addition;
+		} else {
 			salary = _degree_base_pay[0] + (CalculateProfessorAssessmentSum(*professors[i]) / 5) * _max_assesment_addition;
 		}
 		BinaryFile <std::string>salaries((char*) "../storage/Professors.dat");
-		salaries.AddRecord(std::to_string((*professors[i]).getUserName()) + std::to_string(salary));
+		salaries.AddRecord(std::to_string(professors[i]->getUserName()) + std::to_string(salary));
 	}
 }
