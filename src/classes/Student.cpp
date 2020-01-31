17aafb49da1bc8858af8039acbc517322fd4db5f
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-01-31 11:49:28 +0330

    Update Student.cpp

--- src/classes/Student.cpp
+++ src/classes/Student.cpp
@@ -13,19 +13,19 @@ float MyCourse::getScore() const {
 	return score;
 }
 
-void MyCourse::setStatus(short _status) {
+void MyCourse::setStatus(char _status) {
 	status = _status;
 }
 
-short MyCourse::getStatus() const {
+char MyCourse::getStatus() const {
 	return status;
 }
 
-void MyCourse::setAssessmentAnswers(std::vector<short> _assessment_answers) {
+void MyCourse::setAssessmentAnswers(std::vector<char> _assessment_answers) {
 	assessment_answers = _assessment_answers;
 }
 
-std::vector<short> MyCourse::getAssessmentAnswers() const {
+std::vector<char> MyCourse::getAssessmentAnswers() const {
 	return assessment_answers;
 }
 
@@ -35,6 +35,22 @@ void MyCourse::setReview(std::string _review) {
 
 std::string MyCourse::getReview() const {
 	return review;
+}
+
+void MyCourse::setIsObjector(bool _is_objector) {
+	is_objector = _is_objector;
+}
+
+bool MyCourse::getIsObjector() const {
+	return is_objector;
+}
+
+void MyCourse::setObjectionText(std::string _objection_text) {
+	objection_text = _objection_text;
+}
+
+std::string MyCourse::getObjectionText() const {
+	return objection_text;
 }
 
 
@@ -54,11 +70,11 @@ int MyTerm::getno() const {
 	return no;
 }
 
-void MyTerm::setStatus(short _status) {
+void MyTerm::setStatus(char _status) {
 	status = _status;
 }
 
-short MyTerm::getStatus() const {
+char MyTerm::getStatus() const {
 	return status;
 }
 
@@ -108,16 +124,24 @@ void MyTerm::setScoreofCourse(Presented_Course* course, float score) {
 	courses[course].setScore(score);
 }
 
-void MyTerm::setStatusofCourse(Presented_Course* course, short status) {
+void MyTerm::setStatusofCourse(Presented_Course* course, char status) {
 	courses[course].setStatus(status);
 }
 
-void MyTerm::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<short> answers) {
+void MyTerm::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<char> answers) {
 	courses[course].setAssessmentAnswers(answers);
 }
 
 void MyTerm::setReviewofCourse(Presented_Course* course, std::string review) {
 	courses[course].setReview(review);
+}
+
+void MyTerm::setIsObjectorofCourse(Presented_Course* course, bool is_objector) {
+	courses[course].setIsObjector(is_objector);
+}
+
+void MyTerm::setObjectionTextofCourse(Presented_Course* course, std::string objection_text) {
+	courses[course].setObjectionText(objection_text);
 }
 
 
@@ -167,11 +191,11 @@ MyTerm Student::getTerm(int term_no) const {
 	return terms.at(term_no);
 }
 
-void Student::setTermStatus(int term_no, short status) {
+void Student::setTermStatus(int term_no, char status) {
 	terms[term_no].setStatus(status);
 }
 
-short Student::getTermStatus(int term_no) const {
+char Student::getTermStatus(int term_no) const {
 	return terms.at(term_no).getStatus();
 }
 
@@ -183,16 +207,22 @@ Time Student::getTermEnrollmentBeginTime(int term_no) const {
 	return terms.at(term_no).getEnrollmentBeginTime();
 }
 
+std::map<Presented_Course*, MyCourse> Student::getTermCourses(int term_no) const {
+	return terms.at(term_no).getCourses();
+}
+
 int Student::numberofCredits(int term_no) const {
 	return terms.at(term_no).numberofCredits();
 }
 
 void Student::addCourse(Presented_Course* course) {
 	terms[course->getTerm_no()].addCourse(course);
+	// course->addStudent(this);
 }
 
 void Student::removeCourse(Presented_Course* course) {
 	terms[course->getTerm_no()].removeCourse(course);
+	// course->removeStudent(this);
 }
 
 void Student::setCourseProperties(Presented_Course* course, MyCourse properties) {
@@ -211,19 +241,19 @@ float Student::getScoreofCourse(Presented_Course* course) const {
 	return terms.at(course->getTerm_no()).getCourseProperties(course).getScore();
 }
 
-void Student::setStatusofCourse(Presented_Course* course, short status) {
+void Student::setStatusofCourse(Presented_Course* course, char status) {
 	terms[course->getTerm_no()].setStatusofCourse(course, status);
 }
 
-short Student::getStatusofCourse(Presented_Course* course) const {
+char Student::getStatusofCourse(Presented_Course* course) const {
 	return terms.at(course->getTerm_no()).getCourseProperties(course).getStatus();
 }
 
-void Student::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<short> answers) {
+void Student::setAssessmentAnswersofCourse(Presented_Course* course, std::vector<char> answers) {
 	terms[course->getTerm_no()].setAssessmentAnswersofCourse(course, answers);
 }
 
-std::vector<short> Student::getAssessmentAnswersofCourse(Presented_Course* course) const {
+std::vector<char> Student::getAssessmentAnswersofCourse(Presented_Course* course) const {
 	return terms.at(course->getTerm_no()).getCourseProperties(course).getAssessmentAnswers();
 }
 
@@ -235,6 +265,22 @@ std::string Student::getReviewofCourse(Presented_Course* course) const {
 	return terms.at(course->getTerm_no()).getCourseProperties(course).getReview();
 }
 
+void Student::setIsObjectorofCourse(Presented_Course* course, bool is_objector) {
+	terms[course->getTerm_no()].setIsObjectorofCourse(course, is_objector);
+}
+
+bool Student::getIsObjectorofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getIsObjector();
+}
+
+void Student::setObjectionTextofCourse(Presented_Course* course, std::string objection_text) {
+	terms[course->getTerm_no()].setObjectionTextofCourse(course, objection_text);
+}
+
+std::string Student::getObjectionTextofCourse(Presented_Course* course) const {
+	return terms.at(course->getTerm_no()).getCourseProperties(course).getObjectionText();
+}
+
 
 
 
@@ -242,11 +288,11 @@ Tuition_Student::Tuition_Student() {
 
 }
 
-void Tuition_Student::setType(short _type) {
+void Tuition_Student::setType(char _type) {
 	type = _type;
 }
 
-short Tuition_Student::getType() const {
+char Tuition_Student::getType() const {
 	return type;
 }
 
