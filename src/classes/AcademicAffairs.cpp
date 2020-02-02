503be25a840a273105f62480c70857ec2d73089d
shadmehr vasighi <49784686+svasighi@users.noreply.github.com>
2020-02-02 22:26:24 +0330

    added read all to academicaffairs

--- src/classes/AcademicAffairs.cpp
+++ src/classes/AcademicAffairs.cpp
@@ -8,10 +8,23 @@ void AcademicAffairsStaff::setCareer(std::string _career) {
 	career = _career;
 }
 
-void AcademicAffairsStaff::setStudents(std::vector<Student*> _students) {
+void AcademicAffairsStaff::setStudents(std::map<int,Student*> _students) {
 	students = _students;
 }
 
-std::vector<Student*> AcademicAffairsStaff::getStudents() const {
+std::map<int,Student*> AcademicAffairsStaff::getStudents() const {
 	return students;
+}
+void TopAcademicAffairsStaff::changeDepartmentHead(){
+
+}
+void TopAcademicAffairsStaff::readAllStudents() {
+	std::map<int,Student*> TempStudents;
+	BinaryFile<Student> binary_file((char*) "../storage/Students.dat");
+	std::vector<Student*> student = binary_file.FetchAllRecords();
+	
+	for (int i = 0; i < student.size(); i++) {
+		TempStudents.insert(std::pair(student[i]->getUserName(),student[i]));
+	}
+	setStudents(TempStudents);
 }
--- src/include/AcademicAffairs.h
+++ src/include/AcademicAffairs.h
@@ -8,18 +8,19 @@
 class AcademicAffairsStaff : virtual public User {
 protected:
 	std::string career;
-	std::vector<Student*> students;
+	std::map<int,Student*> students;
 public:
 	void setCareer(std::string);
 	std::string getCareer(void) const;
-	void setStudents(std::vector<Student*>);
-	std::vector<Student*> getStudents(void) const;
+	void setStudents(std::map<int,Student*>);
+	std::map<int,Student*> getStudents(void) const;
 };
 
 class TopAcademicAffairsStaff : public AcademicAffairsStaff {
 	
 public:
 	void changeDepartmentHead(Professor* prof); //forward definitions
+	void readAllStudents(void) ;
 };
 
 #endif // ACADEMIC_H
