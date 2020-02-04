860254cd76474f86d6cf0b13149eb7443a8396d9
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-02-04 21:47:27 +0330

    add haveExceptionWithStatus

--- src/include/EnrollmentError.h
+++ src/include/EnrollmentError.h
@@ -11,6 +11,13 @@ public:
 	EnrollmentError(short _errorcode);
 	short getErrorCode() const;
 	virtual std::string what() const = 0;
+public:
+	static constexpr char passed_before = 1;
+	static constexpr char not_passed_prerequisite = 2;
+	static constexpr char not_taken_corequisite = 3;
+	static constexpr char course_time_overlap = 4;
+	static constexpr char exam_time_overlap = 5;
+	static constexpr char full_capacity = 6;
 };
 
 class PassedBefore : public EnrollmentError {
@@ -33,6 +40,7 @@ public:
 	void setDestination(Course* _destination);
 	Course* getDestination() const;
 	std::string what() const;
+	static bool haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Course* _destination, char status);
 };
 
 class NotTakenCorequisite : public EnrollmentError {
@@ -46,6 +54,7 @@ public:
 	void setDestination(Course* _destination);
 	Course* getDestination() const;
 	std::string what() const;
+	static bool haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Course* _destination, char status);
 };
 
 class CourseTimeOverlap : public EnrollmentError {
@@ -59,6 +68,7 @@ public:
 	void setDestination(Presented_Course* _destination);
 	Presented_Course* getDestination() const;
 	std::string what() const;
+	static bool haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Presented_Course* _destination, char status);
 };
 
 class ExamTimeOverlap : public EnrollmentError {
@@ -72,6 +82,7 @@ public:
 	void setDestination(Presented_Course* _destination);
 	Presented_Course* getDestination() const;
 	std::string what() const;
+	static bool haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Presented_Course* _destination, char status);
 };
 
 class FullCapacity : public EnrollmentError {
@@ -81,6 +92,7 @@ public:
 	void setSource(Presented_Course* _source);
 	Presented_Course* getSource() const;
 	std::string what() const;
+	static bool haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, char status);
 };
 
 #endif // ENROLLMENTERROR_H
