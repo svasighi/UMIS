f0b4676e98948939edb3d34410dabe6064b775b4
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-02-04 21:48:18 +0330

    add haveExceptionWithStatus

--- src/classes/EnrollmentError.cpp
+++ src/classes/EnrollmentError.cpp
@@ -11,7 +11,7 @@ short EnrollmentError::getErrorCode() const {
 
 
 PassedBefore::PassedBefore(Presented_Course* _source)
-	: source(_source), EnrollmentError(1) {}
+	: source(_source), EnrollmentError(EnrollmentError::passed_before) {}
 
 void PassedBefore::setSource(Presented_Course* _source) {
 	source = _source;
@@ -29,7 +29,7 @@ std::string PassedBefore::what() const {
 
 
 NotPassedPrerequisite::NotPassedPrerequisite(Presented_Course* _source, Course* _destination)
-	: source(_source), destination(_destination), EnrollmentError(2) {}
+	: source(_source), destination(_destination), EnrollmentError(EnrollmentError::not_passed_prerequisite) {}
 
 void NotPassedPrerequisite::setSource(Presented_Course* _source) {
 	source = _source;
@@ -55,7 +55,7 @@ std::string NotPassedPrerequisite::what() const {
 
 
 NotTakenCorequisite::NotTakenCorequisite(Presented_Course* _source, Course* _destination)
-	: source(_source), destination(_destination), EnrollmentError(3) {}
+	: source(_source), destination(_destination), EnrollmentError(EnrollmentError::not_taken_corequisite) {}
 
 void NotTakenCorequisite::setSource(Presented_Course* _source) {
 	source = _source;
@@ -81,7 +81,7 @@ std::string NotTakenCorequisite::what() const {
 
 
 CourseTimeOverlap::CourseTimeOverlap(Presented_Course* _source, Presented_Course* _destination)
-	: source(_source), destination(_destination), EnrollmentError(4) {}
+	: source(_source), destination(_destination), EnrollmentError(EnrollmentError::course_time_overlap) {}
 
 void CourseTimeOverlap::setSource(Presented_Course* _source) {
 	source = _source;
@@ -107,7 +107,7 @@ std::string CourseTimeOverlap::what() const {
 
 
 ExamTimeOverlap::ExamTimeOverlap(Presented_Course* _source, Presented_Course* _destination)
-	: source(_source), destination(_destination), EnrollmentError(5) {}
+	: source(_source), destination(_destination), EnrollmentError(EnrollmentError::exam_time_overlap) {}
 
 void ExamTimeOverlap::setSource(Presented_Course* _source) {
 	source = _source;
@@ -133,7 +133,7 @@ std::string ExamTimeOverlap::what() const {
 
 
 FullCapacity::FullCapacity(Presented_Course* _source)
-	: source(_source), EnrollmentError(6) {}
+	: source(_source), EnrollmentError(EnrollmentError::full_capacity) {}
 
 void FullCapacity::setSource(Presented_Course* _source) {
 	source = _source;
@@ -145,4 +145,65 @@ Presented_Course* FullCapacity::getSource() const {
 
 std::string FullCapacity::what() const {
 	return "Error #" + std::to_string(errorcode) + " : Course number " + std::to_string(source->getCompleteID()) + " has full capacity";
+}
+
+
+
+bool NotPassedPrerequisite::haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Course* _destination, char status) {
+	for (const auto& exc : exceptions) {
+		if (exc.first->getErrorCode() == EnrollmentError::not_passed_prerequisite) {
+			NotPassedPrerequisite* e = dynamic_cast<NotPassedPrerequisite*>(exc.first);
+			if (e->getSource()->isSameWith(_source) && e->getDestination()->haveSameID(_destination) && exc.second == status) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool NotTakenCorequisite::haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Course* _destination, char status) {
+	for (const auto& exc : exceptions) {
+		if (exc.first->getErrorCode() == EnrollmentError::not_taken_corequisite) {
+			NotTakenCorequisite* e = dynamic_cast<NotTakenCorequisite*>(exc.first);
+			if (e->getSource()->isSameWith(_source) && e->getDestination()->haveSameID(_destination) && exc.second == status) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool CourseTimeOverlap::haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Presented_Course* _destination, char status) {
+	for (const auto& exc : exceptions) {
+		if (exc.first->getErrorCode() == EnrollmentError::course_time_overlap) {
+			CourseTimeOverlap* e = dynamic_cast<CourseTimeOverlap*>(exc.first);
+			if (e->getSource()->isSameWith(_source) && e->getDestination()->isSameWith(_destination) && exc.second == status) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool ExamTimeOverlap::haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, Presented_Course* _destination, char status) {
+	for (const auto& exc : exceptions) {
+		if (exc.first->getErrorCode() == EnrollmentError::exam_time_overlap) {
+			ExamTimeOverlap* e = dynamic_cast<ExamTimeOverlap*>(exc.first);
+			if (e->getSource()->isSameWith(_source) && e->getDestination()->isSameWith(_destination) && exc.second == status) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool FullCapacity::haveExceptionWithStatus(const std::map<EnrollmentError*, char>& exceptions, Presented_Course* _source, char status) {
+	for (const auto& exc : exceptions) {
+		if (exc.first->getErrorCode() == EnrollmentError::full_capacity) {
+			if (dynamic_cast<FullCapacity*>(exc.first)->getSource()->isSameWith(_source) && exc.second == status) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
