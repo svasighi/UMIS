9907d195e2a9976642dffc7bae2f792ebaa6b820
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-02-07 12:23:29 +0330

    Create studentwindow.h

---
+++ students/studentwindow.h
@@ -0,0 +1,32 @@
+#ifndef STUDENTWINDOW_H
+#define STUDENTWINDOW_H
+
+#include <QWidget>
+
+namespace Ui {
+class StudentWindow;
+}
+
+class StudentWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit StudentWindow(QWidget *parent = nullptr);
+    ~StudentWindow();
+
+private:
+    Ui::StudentWindow *ui;
+
+private slots:
+    void on_TB_Signout_clicked();
+    void on_TW_Program_tabCloseRequested(int index);
+    void on_PB_PresentedCourses_clicked();
+    void on_PB_WeekSchedule_clicked();
+    void on_PB_ExamSchedule_clicked();
+    void on_PB_Tuition_clicked();
+    void on_PB_Assessment_clicked();
+    void on_PB_FullInformatin_clicked();
+};
+
+#endif // STUDENTWINDOW_H
