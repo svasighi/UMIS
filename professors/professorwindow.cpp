02ad73c5854a7d726e88f0190802d0bd4daa8b74
mohseni1008 <59229565+mohseni1008@users.noreply.github.com>
2020-02-07 16:17:30 +0330

    Update professorwindow.cpp

--- professors/professorwindow.cpp
+++ professors/professorwindow.cpp
@@ -1,3 +1,4 @@
+#include "../mainwindow.h"
 #include "professorwindow.h"
 #include "ui_professorview.h"
 #include "professorpresentedcoursesform.h"
@@ -17,7 +18,17 @@ ProfessorWindow::~ProfessorWindow()
 }
 void ProfessorWindow::on_TB_Signout_clicked()
 {
-    emit want2close();
+    MainWindow *wmain = new MainWindow();
+    wmain->setGeometry(this->geometry());
+    if (this->isMaximized())
+    {
+        wmain->showMaximized();
+    }
+    if (this->isFullScreen())
+    {
+        wmain->showFullScreen();
+    }
+    wmain->show();
     this->close();
     ui->TW_ProfMenu->setCurrentIndex(0);
 }
@@ -33,35 +44,45 @@ void ProfessorWindow::on_TW_Program_tabCloseRequested(int index)
 void ProfessorWindow::on_PB_PresentedCourses_clicked()
 {
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("دروس ارائه شده در ترم"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_EnterScores(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("ثیت نمرات"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_StudentsObjections(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("اعتراضات دانشجویان"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_SrudentsList(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("مشاهده لیست دانشجویان"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_Exceptions(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("فرم های استثنا"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_PreEnrollmentStats(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("وضعیت ثبت نام مقدماتی"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_AddCourse(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("افزودن درس"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_AddPresentedCourse(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("افزودن درس ارائه شده"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_ProfessorsAssesment(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("آمار ارزشیابی اساتید"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_CalculateSalary(){
     ui->TW_Program->addTab(new ProfessorPresentedCoursesForm(), QString("محاسبه حقوق"));
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
 void ProfessorWindow::on_PB_ProfessorsList(){
     ui->TW_Program->addTab(new ProfessorsListForm(), QString("لیست اساتید"));
-
+    ui->TW_Program->setCurrentIndex(ui->TW_Program->count() - 1);
 }
